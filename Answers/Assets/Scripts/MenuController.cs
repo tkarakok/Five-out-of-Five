b0e88b2a910d9abf6f94744c7f3ac81d90a89749
Tunahan <59062912+tkarakok@users.noreply.github.com>
2021-10-22 02:25:58 +0300

    v2

---
+++ Answers/Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject startPanel, choosePanel, settingsPanel;
+
+    public void StartButtonFunction(){
+        startPanel.SetActive(false);
+        choosePanel.SetActive(true);
+    }
+     public void SettingsButtonFunction(){
+        startPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+    public void SettingsBackButton(){
+        settingsPanel.SetActive(false);
+        startPanel.SetActive(true);
+    }
+    public void QuitButtonFunction(){
+        Application.Quit();
+    }
+
+
+}
--- Answers/Assets/Scripts/QuestionController.cs
+++ Answers/Assets/Scripts/QuestionController.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 
 public class QuestionController : MonoBehaviour
 {
-    [SerializeField] Text questionText, choiceA, choiceB, choiceC, choiceD;
+    [SerializeField] Text questionText, choiceA, choiceB, choiceC, choiceD, pointText;
     [SerializeField] Button nextQuestionButton;
-    [SerializeField] Button[] buttons;
-    [SerializeField] GameObject questionPanel, trueAnswerPanel, wrongAnswerPanel, choosePanel;
+    [SerializeField] Button[] choiceButtons;
+    [SerializeField] Button[] categoryButtons;
+    [SerializeField] GameObject questionPanel, trueAnswerPanel, wrongAnswerPanel, choosePanel, startPanel, welldonePanel;
+    int point, turn = 0, choiceNumber;
     Color color;
     QuestionList questionList;
     Question question;
@@ -16,7 +18,8 @@ public class QuestionController : MonoBehaviour
 
     private void Start()
     {
-        color = buttons[0].gameObject.GetComponent<Image>().color;
+        point = 0;
+        color = choiceButtons[0].gameObject.GetComponent<Image>().color;
         questionList = GetComponent<QuestionList>();
     }
 
@@ -30,33 +33,45 @@ public class QuestionController : MonoBehaviour
 
         if (categoryID == 1)
         {
+            choiceNumber = 1;
             question = questionList.Culture[Random.Range(0, questionList.Culture.Count)];
             PrintQuestion();
+            categoryButtons[0].interactable = false;
         }
         else if (categoryID == 2)
         {
+            choiceNumber = 2;
             question = questionList.Math[Random.Range(0, questionList.Math.Count)];
             PrintQuestion();
+            categoryButtons[1].interactable = false;
         }
         else if (categoryID == 3)
         {
+            choiceNumber = 3;
             question = questionList.Geography[Random.Range(0, questionList.Geography.Count)];
             PrintQuestion();
+            categoryButtons[2].interactable = false;
         }
         else if (categoryID == 4)
         {
+            choiceNumber = 4;
             question = questionList.Science[Random.Range(0, questionList.Science.Count)];
             PrintQuestion();
+            categoryButtons[3].interactable = false;
         }
         else if (categoryID == 5)
         {
+            choiceNumber = 5;
             question = questionList.Sport[Random.Range(0, questionList.Sport.Count)];
             PrintQuestion();
+            categoryButtons[2].interactable = false;
         }
         else if (categoryID == 6)
         {
+            choiceNumber = 6;
             question = questionList.Cinema[Random.Range(0, questionList.Cinema.Count)];
             PrintQuestion();
+            categoryButtons[5].interactable = false;
         }
     }
 
@@ -76,28 +91,28 @@ public class QuestionController : MonoBehaviour
         if (choiceID == 1)
         {
             _choice = Choice.aChoice;
-            _selectedButton = buttons[0];
+            _selectedButton = choiceButtons[0];
             coroutine = AnswerControl(_choice, _selectedButton);
             StartCoroutine(coroutine);
         }
         else if (choiceID == 2)
         {
             _choice = Choice.bChoice;
-            _selectedButton = buttons[1];
+            _selectedButton = choiceButtons[1];
             coroutine = AnswerControl(_choice, _selectedButton);
             StartCoroutine(coroutine);
         }
         else if (choiceID == 3)
         {
             _choice = Choice.cChoice;
-            _selectedButton = buttons[2];
+            _selectedButton = choiceButtons[2];
             coroutine = AnswerControl(_choice, _selectedButton);
             StartCoroutine(coroutine);
         }
         else if (choiceID == 4)
         {
             _choice = Choice.dChoice;
-            _selectedButton = buttons[3];
+            _selectedButton = choiceButtons[3];
             coroutine = AnswerControl(_choice, _selectedButton);
             StartCoroutine(coroutine);
         }
@@ -105,44 +120,62 @@ public class QuestionController : MonoBehaviour
     }
     void ButtonRefresh()
     {
-        for (int i = 0; i < buttons.Length; i++)
+        for (int i = 0; i < choiceButtons.Length; i++)
         {
-            buttons[i].gameObject.GetComponent<Image>().color = color;
-            buttons[i].interactable = true;
+            choiceButtons[i].gameObject.GetComponent<Image>().color = color;
+            choiceButtons[i].interactable = true;
         }
-       
     }
 
     public void NewChoose()
     {
         trueAnswerPanel.SetActive(false);
-        questionPanel.SetActive(false);
-        choosePanel.SetActive(true);
+        GiveQuestion(choiceNumber);
     }
 
+    IEnumerator Welldone()
+    {
+        welldonePanel.SetActive(true);
+        yield return new WaitForSeconds(2);
+        welldonePanel.SetActive(false);
+
+    }
     IEnumerator AnswerControl(Choice choice, Button selectedButton)
     {
-        for (int i = 0; i < buttons.Length; i++)
+        for (int i = 0; i < choiceButtons.Length; i++)
         {
-            buttons[i].interactable = false;
+            choiceButtons[i].interactable = false;
         }
         selectedButton.gameObject.GetComponent<Image>().color = Color.blue;
         yield return new WaitForSeconds(1);
 
         if (choice == question.trueAnswer)
         {
+            point += 10;
+            if (turn == 4)
+            {
+                choiceNumber = 0;
+                questionPanel.SetActive(false);
+                StartCoroutine(Welldone());
+                choosePanel.SetActive(true);
+                turn = 0;
+            }
+            else
+            {
+                turn++;
+                trueAnswerPanel.SetActive(true);
+            }
+            pointText.text = point.ToString();
             selectedButton.gameObject.GetComponent<Image>().color = Color.green;
-            trueAnswerPanel.SetActive(true);
             nextQuestionButton.interactable = true;
         }
         else
         {
             selectedButton.gameObject.GetComponent<Image>().color = Color.red;
-            wrongAnswerPanel.SetActive(true );
+            wrongAnswerPanel.SetActive(true);
             yield return new WaitForSeconds(1);
             wrongAnswerPanel.SetActive(false);
             selectedButton.gameObject.GetComponent<Image>().color = color;
-
             ButtonRefresh();
         }
 
